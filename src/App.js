e55a76daab8f795463ceda0c9194aa288df50fe3
TaylorMasiello <tmasiello@echobind.com>
2021-08-06 11:10:48 -0700

    Router implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import { render } from "react-dom";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRoter as Router, Route, Switch, Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <header>
+          <Link to="/">
+            <h1>Adopt Me!</h1>
+          </Link>
+        </header>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
 
-render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
